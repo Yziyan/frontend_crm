e8b874cd0a00983867170d1a8155861b7ff41f83
zhiyan <1533460130@qq.com>
2022-07-17 20:58:07 +0800

    '修改服务器地址'

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -7,7 +7,7 @@ const TIMEOUT: number = 10000;
 if (process.env.NODE_ENV === "development") {
     BASE_URL = "http://localhost:8888";
 } else if (process.env.NODE_ENV === "production") {
-    BASE_URL = "http://159.75.71.126:8888";
+    BASE_URL = "http://81.71.151.101:8888";
 } else {
     BASE_URL = "http://192.168.1.8:8888";
 }
